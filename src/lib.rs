e78bcdf9256427feadc65a1ef0d9b0f9ebd8d187
Liam Kinne <liam@kinne.io>
2023-11-21 03:27:38 +1000

    device selected feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,21 @@
 #![no_std]
 
+#[cfg(feature = "device-selected")]
 pub use embedded_hal as hal;
 
+#[cfg(feature = "pac25140")]
 pub use pac25::pac25140 as pac;
 
+#[cfg(feature = "device-selected")]
 pub mod gpio;
+#[cfg(feature = "device-selected")]
 pub mod prelude;
 
+#[cfg(feature = "device-selected")]
 mod sealed {
     pub trait Sealed {}
 }
+#[cfg(feature = "device-selected")]
 pub(crate) use sealed::Sealed;
 
 fn stripped_type_name<T>() -> &'static str {
