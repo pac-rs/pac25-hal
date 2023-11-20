107f7a042b2d50a583c3f18dad5f00e69dcf1173
Liam Kinne <liam@kinne.io>
2023-11-21 03:44:43 +1000

    add input example

---
+++ examples/input.rs
@@ -0,0 +1,28 @@
+//! Reads the state of an input pin
+
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use defmt_rtt as _;
+use pac25_hal as hal;
+use panic_halt as _;
+
+use hal::pac;
+use hal::prelude::*;
+
+#[entry]
+fn main() -> ! {
+    defmt::println!("Starting reading.");
+
+    let dp = pac::Peripherals::take().unwrap();
+    let gpiod = dp.GPIOD.split();
+    let mut input = gpiod.pd4.into_pull_down_input();
+
+    loop {
+        defmt::println!("State: {=bool}", input.is_high());
+        for _ in 0..5_000 {
+            continue;
+        }
+    }
+}
