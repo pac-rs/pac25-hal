ae63e941fe3bbc517ca5b2e6e59f1973f4c21326
Liam Kinne <liam@kinne.io>
2023-11-21 18:39:18 +1000

    cleanup

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -16,18 +16,18 @@ fn main() -> ! {
     defmt::println!("Starting Blinky!");
 
     let dp = pac::Peripherals::take().unwrap();
-
     let gpiod = dp.GPIOD.split();
-
     let mut led = gpiod.pd4.into_push_pull_output();
 
     loop {
         defmt::println!("Blink!");
+
         if led.is_set_high() {
             led.set_low()
         } else {
             led.set_high()
         }
+
         for _ in 0..5_000 {
             continue;
         }
